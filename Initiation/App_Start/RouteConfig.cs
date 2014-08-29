937c772529f7a47d74a94468c4d2ca668fb700bb
DT420 <davtremblay@hotmail.com>
2014-08-29 12:39:01 -0400

    Premier commit

---
+++ Initiation/App_Start/RouteConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Initiation
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Frontpage","", new { controller = "Home", action = "Index"});
+
+            routes.MapRoute("Form", "Yo", new { controller = "Form", action = "Form" });
+        }
+    }
+}
---
+++ Initiation/Controllers/FormController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Initiation.Controllers
+{
+    public class FormController : Controller
+    {
+        public ActionResult Form()
+        {
+            ViewBag.ImgUrl = "http://www.publicdomainpictures.net/pictures/50000/nahled/smiley-silhouette.jpg";
+            ViewBag.Message1 = "Yo";
+            ViewData["Message2"] = "What's up?";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Form(string nom, string email,string commentaire)
+        {
+            return View();
+        }
+
+    }
+}
